385e64d2ae1a9c4f7d14a18b60e7efe199021d76
Iconoclast209 <mrpresidentsmith@gmail.com>
2020-03-27 14:29:40 -0700

    Upgraded Unity Version. Started a prototype of a simple card.

---
+++ City Builder/Assets/Scripts/Card.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Card : MonoBehaviour
+{
+    // What is the name of this card?
+    [SerializeField] string cardName;
+    // Which Player is the Owner of this card?
+    [SerializeField] int owner;
+    // What die roll activates this card?
+    [SerializeField] int activationValue;
+    // How many coins does this card take to build?
+    [SerializeField] int buildCost;
+    // How many coins does this card generate
+    [SerializeField] int revenueGenerated;
+    // When does this card generate revenue? All turns or active player only?
+    [SerializeField] bool activeOnAllTurns;
+    // The picture of the card.
+    Image cardImage;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
