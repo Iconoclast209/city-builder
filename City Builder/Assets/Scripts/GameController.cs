7986314185997bb026c97a0b58ba4fc138826549
Iconoclast209 <mrpresidentsmith@gmail.com>
2021-03-18 20:58:17 -0700

    Updated to Unity 2019.4.20 and started to implement Card class and Player class.  Also updated .gitignore

--- City Builder/Assets/Scripts/GameController.cs
+++ City Builder/Assets/Scripts/GameController.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //Need this to be a singleton
+
+    // Number of Players for this game, 2-4 valid
+    [SerializeField] int numberOfPlayers = 2;
+    [SerializeField] int activePlayer = 0;
+
+
+    
+    public void StartGame()
     {
-        
+        //Determine number of players
+        //Create the appropriate number of players
+        for (int i = 0; i < numberOfPlayers; i++)
+        {
+            //Create Player
+        }             
+
+        //Set player number 1 to active
+        activePlayer = 1;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
---
+++ City Builder/Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    //Which player is this?  valid numbers 1-4
+    [SerializeField] int playerNumber;
+
+    //How many coins does this player have?  Default starting value is 3.
+    [SerializeField] int coins = 3;
+
+    //Color to help represent player in UI.
+    [SerializeField] Color playerColor;
+
+    //Landmark Status
+    bool ownsTrainStation = false;
+    bool ownsShoppingMall = false;
+    bool ownsAmusementPark = false;
+    bool ownsRadioTower = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
